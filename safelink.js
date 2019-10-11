b21818bd9bf32a13ef07c93669ad1b3a1e1588ba
mediatech20 <56314377+mediatech20@users.noreply.github.com>
2019-10-11 19:52:04 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -21,11 +21,9 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="lorem-ipsum-dolo.html"}
-if(x=="2"){xxx="sed-sagittis-neque.html"}
-if(x=="3"){xxx="blog-number-one.html"}
-if(x=="4"){xxx="soccer-gamers.html"}
-if(x=="5"){xxx="go-to-url.html"}a_to_ve[i].href="https://maycyber-download-safelink.blogspot.com/2017/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="safelink.html"}
+if(x=="2"){xxx="3-bahaya-menggunakan-vpn-secara-gratis.html"}
+if(x=="3"){xxx="go-to-url.html"}a_to_ve[i].href="https://free-software-techno.blogspot.com/2019/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
